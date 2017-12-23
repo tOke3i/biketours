e196cd0051e4876b5f8119d92392e20d593e5f66
pavlyutspv <pavlyutspv@qulix.com>
2017-12-23 23:58:14 +0300

    0.3

--- app/js/template.js
+++ app/js/template.js
@@ -1,18 +1,18 @@
 jQuery(function($) {
 
 	/* =============== SHOW / HIDE FORM SEARCH =============== */
-	$("header .nino-search, #nino-searchForm .nino-close").click(function(){
-		$("#nino-searchForm").toggleClass("open");
+	$("header .bike-search, #bike-searchForm .bike-close").click(function(){
+		$("#bike-searchForm").toggleClass("open");
 	});
 
 	/* =============== SHOW / HIDE GOOGLE MAP =============== */
-	$("#nino-map .nino-sectionHeading").click(function(){
-		$("#nino-map").toggleClass("showMap");
+	$("#bike-map .bike-sectionHeading").click(function(){
+		$("#bike-map").toggleClass("showMap");
 		$(this).find(".text").toggle();
 	});
 
 	/* =============== TESTIMONIAL SLIDER =============== */
-	$('.nino-testimonialSlider').unslider({
+	$('.bike-testimonialSlider').unslider({
 		nav: false,
 		arrows: {
 			//  Unslider default behaviour
@@ -22,36 +22,36 @@ jQuery(function($) {
 	});
 
 	/* =============== CUSTOM SCROLLBAR STYLE =============== */
-	$("#nino-whatWeDo .panel-body").mCustomScrollbar({
+	$("#bike-whatWeDo .panel-body").mCustomScrollbar({
 		theme:"default"
 	});
 
 	/* =============== MAKE MAIN MENU STICKED ON TOP WHEN SCROLL =============== */
 	$(window).scroll(function () {
-		if ($(this).scrollTop() == $('#nino-header').height() || $(this).scrollTop() > $('#nino-header').height()) {
-			$('body').addClass("nino-fixed-nav");
-			$('body').css('padding-top', $('#nino-navbar').height() + 'px');
+		if ($(this).scrollTop() == $('#bike-header').height() || $(this).scrollTop() > $('#bike-header').height()) {
+			$('body').addClass("bike-fixed-nav");
+			$('body').css('padding-top', $('#bike-navbar').height() + 'px');
 		} else {
-			$('body').removeClass("nino-fixed-nav");
+			$('body').removeClass("bike-fixed-nav");
 			$('body').css('padding-top', 0);
 		}
 	});
-	
-	/* =============== ISOTOP =============== */	
+
+	/* =============== ISOTOP =============== */
 	$(window).load(function(){
-		$portfolio = $('.nino-portfolioItems');
+		$portfolio = $('.bike-portfolioItems');
 		$portfolio.isotope({
 			itemSelector : 'li',
 			layoutMode : 'masonry'
 		});
 	});
-	
-	/* =============== PORTFOLIO HOVER EFFECT =============== */
-	$('.nino-portfolioItems > li').each( function() { $(this).hoverdir(); } );
 
-	
+	/* =============== PORTFOLIO HOVER EFFECT =============== */
+	$('.bike-portfolioItems > li').each( function() { $(this).hoverdir(); } );
+
+
 	/* =============== SMOOTH SCROOL EFFECT =============== */
-	$('#nino-navbar ul li a').on('click',function (e) {
+	$('.bike-menuItem ul li a').on('click',function (e) {
 	    e.preventDefault();
 	    var target = this.hash,
 	    $target = $(target);
@@ -62,18 +62,18 @@ jQuery(function($) {
 	        window.location.hash = target;
 	    });
 	});
-	
+
 	/* =============== SHOW / HIDE GO TO TOP =============== */
 	/* Check to see if the window is top if not then display go top button */
 	$(window).scroll(function(){
 		if ($(this).scrollTop() > 200) {
-			$('#nino-scrollToTop').fadeIn();
+			$('#bike-scrollToTop').fadeIn();
 		} else {
-			$('#nino-scrollToTop').fadeOut();
+			$('#bike-scrollToTop').fadeOut();
 		}
 	});
 	/* Click event to scroll to top */
-	$('#nino-scrollToTop').click(function(){
+	$('#bike-scrollToTop').click(function(){
 		$('html, body').animate({scrollTop : 0},800);
 		return false;
 	});
